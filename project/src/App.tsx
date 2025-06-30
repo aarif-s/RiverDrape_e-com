8e5dd2c02c45fb6856cdab469a1f8bc281ac41f9
aarif-s <alijmi0009@gmail.com>
2025-07-01 02:49:00 +0530

    Update App.tsx
    aabid

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -3,7 +3,7 @@
 const   App = () => {
   return (
     <div className="App">
-      <h1 className="text-red-500 font-bold text-3xl">Welcome to the React App</h1>
+      <h1 className="text-red-500 font-bold text-3xl">Welcome Aabid</h1>
       <p>This is a simple React application.</p>
     </div>
   );
