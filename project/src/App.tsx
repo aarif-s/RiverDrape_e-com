f65a734c0aa940e9b0ae244a89259c2ba1e8e214
aarif-s <alijmi0009@gmail.com>
2025-07-01 02:42:58 +0530

    update1

---
+++ project/src/App.tsx
@@ -0,0 +1,12 @@
+ 
+
+const   App = () => {
+  return (
+    <div className="App">
+      <h1 className="text-red-500 font-bold text-3xl">Welcome to the React App</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+};
+
+export default App;
